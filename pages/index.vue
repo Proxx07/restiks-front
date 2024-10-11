470221bcd30a50ed691611588486ef59c32fa330
z.pulatov <z.neotech97@gmail.com>
2024-10-11 21:00:56 +0500

    [WIP] Data fetching

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/regionmenu', { query: { RegionId: 0, Language: 2 }, server: false });
+const { data } = await useFetch('/api/regionmenu', { query: { RegionId: 0, Language: 2 } });
 const categories = computed(() => data?.value?.categories.filter(i => !i.parentId)) ?? [];
 const activeFolder = ref<string>('');
-const foldersAndSubfolders = computed(() => data.value?.categories.filter(i => i.parentId)) ?? [];
-
-const prods = computed(() => foldersAndSubfolders.value?.filter(i => i.parentId === activeFolder.value)) ?? [];
-
-const cartProds = computed(() => data?.value?.products ?? []);
+const products = computed(() => data?.value?.products.filter(prod => prod.category.id === activeFolder.value) ?? []);
 </script>
 
 <template>
@@ -17,11 +13,13 @@ const cartProds = computed(() => data?.value?.products ?? []);
       </Button>
     </div>
     <hr>
-    prods: <pre> {{ prods }} </pre>
-    <hr>
-    hz: <pre> {{ foldersAndSubfolders }} </pre>
-    <hr>
-    cartProds: <pre> {{ cartProds }} </pre>
+
+    <div v-for="product in products" :id="product.id" :key="product.id" class="shadow">
+      <b>{{ product.name }}</b>
+      <pre style="white-space: pre-wrap;">
+        {{ product }}
+      </pre>
+    </div>
   </div>
 </template>
 
